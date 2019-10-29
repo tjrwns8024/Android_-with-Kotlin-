c125c27e4f29013416c5cf7b4ffe926c1e7ee3aa
seokjun <tjrwns8024@naver.com>
2019-10-29 23:10:51 +0900

    ViewPager[app base]

---
+++ ViewPager/app/src/main/java/com/example/viewpager/MainFragment.kt
@@ -0,0 +1,11 @@
+package com.example.viewpager
+
+import androidx.fragment.app.Fragment
+
+open class MainFragment : Fragment()
+{
+    override fun newInstance():Fragment
+    {
+        
+    }
+}
