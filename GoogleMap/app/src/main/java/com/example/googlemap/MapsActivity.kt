15d3568b80773c534949c173078828a83a38110f
seokjun <tjrwns8024@naver.com>
2019-11-12 23:12:15 +0900

    Markers[add]

--- GoogleMap/app/src/main/java/com/example/googlemap/MapsActivity.kt
+++ GoogleMap/app/src/main/java/com/example/googlemap/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.googlemap
 
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +12,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 @Suppress("UNREACHABLE_CODE")
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     override fun onMarkerClick(p0: Marker?) = false
 
     //구글 맵 조작을 위한 객체 생성
@@ -24,7 +27,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMarkerC
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
 
-    companion object{
+    companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 
@@ -52,42 +55,56 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMarkerC
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-   /*         // Add a marker in Sydney and move the camera
-            val daejeon = LatLng(36.35,127.38)
-            map.addMarker(MarkerOptions().position(daejeon).title("Marker in 대전"))
-            map.moveCamera(CameraUpdateFactory.newLatLng(daejeon))
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(daejeon,12.0f))*/
+        /*         // Add a marker in Sydney and move the camera
+                 val daejeon = LatLng(36.35,127.38)
+                 map.addMarker(MarkerOptions().position(daejeon).title("Marker in 대전"))
+                 map.moveCamera(CameraUpdateFactory.newLatLng(daejeon))
+                 map.moveCamera(CameraUpdateFactory.newLatLngZoom(daejeon,12.0f))*/
 
         map.getUiSettings().isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
 
         setUpMap()
     }
-    private fun setUpMap(){
-        if(ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ){
-            ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
             return
-
-            // isMyLocationEnabled = true는
-            // 사용자의 위치에 옅은 파란색 점을 그리는 my-location 계층을 활성화한다.
-            // 또한 지도에 버튼을 추가하여, 탭할 때, 지도의 중심을 사용자의 위치에 맞춘다
-            map.isMyLocationEnabled = true
-
-            //현재 사용가능한 가장 최근의 위치를 제공한다.
-            fusedLocationClient.lastLocation.addOnSuccessListener(this){
-                location ->
-
-                //가장 최근의 위치를 검색할 수 있으면 카메라를 사용자의 현재 위치로 이동하십시오.
-                if(location!=null)
-                {
-                    lastLocation = location
-                    val currentLatLng = LatLng(location.latitude,location.longitude)
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,12f))
-                }
-            }
-
         }
+
+        // isMyLocationEnabled = true는
+        // 사용자의 위치에 옅은 파란색 점을 그리는 my-location 계층을 활성화한다.
+        // 또한 지도에 버튼을 추가하여, 탭할 때, 지도의 중심을 사용자의 위치에 맞춘다
+        map.isMyLocationEnabled = true
+
+        //현재 사용가능한 가장 최근의 위치를 제공한다.
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+
+            //가장 최근의 위치를 검색할 수 있으면 카메라를 사용자의 현재 위치로 이동하십시오.
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+
+    }
+
+    private fun placeMarkerOnMap(location: LatLng) {
+        //MarkerOptions 객체 생성 및 사용자의 현재 위치를 마커 위치로 설정
+        val markerOptions = MarkerOptions().position(location)
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(resources, R.drawable.ic_user_location)))
+        //지도에 마커 추가
+        map.addMarker(markerOptions)
     }
 }
