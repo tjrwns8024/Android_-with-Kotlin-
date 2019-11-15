f100f89cb1adaa1c308818a6f5cd84f42603b3ef
seokjun <tjrwns8024@naver.com>
2019-11-15 16:26:29 +0900

    ResultActivity

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -8,5 +8,6 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
     }
 }
