03c9e7c25ffa475648469da5c7142f82d8859d6a
seokjun <tjrwns8024@naver.com>
2019-11-15 18:59:06 +0900

    final

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.bmicalculator
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+
+        resultButton.setOnClickListener {
+            saveData(heightEditText.text.toString().toInt(),
+                    weightEditText.text.toString().toInt())
+
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString(),
+                "height" to heightEditText.text.toString()
+            )
+        }
     }
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = getSharedPreferences("saveData", MODE_PRIVATE)
+        val editor:SharedPreferences.Editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+
+    private fun loadData(){
+        val pref = getSharedPreferences("saveData", MODE_PRIVATE)
+        val height = pref.getInt("KEY_HEIGHT", 0)
+        val weight = pref.getInt("KEY_WEIGHT", 0)
+
+        if(height != 0 && weight != 0){
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
+    }
+
 }
