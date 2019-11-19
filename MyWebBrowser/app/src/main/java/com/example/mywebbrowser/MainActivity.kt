69f0e800022ada2c0e0507d6aa2bb3d3493bb267
seokjun <tjrwns8024@naver.com>
2019-11-19 22:49:38 +0900

    SearchWebView

--- MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,5 +38,44 @@ class MainActivity : AppCompatActivity() {
         }else{
             super.onBackPressed()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_google, R.id.action_home ->{
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver ->{
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_daum ->{
+                webView.loadUrl("http://www.daum.net")
+                return true
+            }
+            R.id.action_call ->{
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:010-xxxx-xxxx")
+                if(intent.resolveActivity(packageManager)!=null)
+                    startActivity(intent)
+
+                return true
+            }
+            R.id.action_send_text ->{
+                //문자 보내기
+                return  true
+            }
+            R.id.action_email->{
+                //이메일 보내기
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
